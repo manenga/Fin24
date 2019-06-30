b1c912d1917e713b99be9b1f107b761c4c28e983
Manenga Mungandi <manengam@gmail.com>
2019-06-30 16:44:41 +0200

    Initial Commit

---
+++ Fin24Tests/Fin24Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Fin24Tests.swift
+//  Fin24Tests
+//
+//  Created by Manenga Mungandi on 2019/06/30.
+//  Copyright © 2019 Manenga Mungandi. All rights reserved.
+//
+
+import XCTest
+@testable import Fin24
+
+class Fin24Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
